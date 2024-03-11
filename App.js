910085b41f14ccbc65776c4251240087880b2791
Toktomamatova <asixqss@icloud.com>
2024-03-11 22:55:49 +0600

    Update App.js

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import About from "./components/about/About";
 import Title from "./components/title/Title";
 function App() {
   const titleData={title:"Аманай",subtitle:"34-03"};
-  const aboutData={title:"Центр Geeks",body:"Переходите поссылке ниже↓",link:"https://geeks.kg/"};
+  const aboutData={title:"Центр Geeks",body:"Переходите по ссылке ниже↓",link:"https://geeks.kg/"};
   return (
       <div>
         <About {...aboutData} />
